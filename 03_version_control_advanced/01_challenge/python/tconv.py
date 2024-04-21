335fad81bfcdb937d1fe0d14523e903151d75aab
Satomi Takahashi <satomi.takahashi@woven.toyota>
2024-04-21 18:22:16 +0900

    added convert_fahrenheit_to_celsius in tconv.py

--- 03_version_control_advanced/01_challenge/python/tconv.py
+++ 03_version_control_advanced/01_challenge/python/tconv.py
@@ -12,17 +12,3 @@ def convert_fahrenheit_to_celsius():
     celsius = 5/9 * ( fehrenheit - 32)
     print(f"The Fehrenheit temprature {fehrenheit} you entered is {celsius} in Celsius.")
 
-def main():
-    convert_celsius_to_fahrenheit()
-    convert_fahrenheit_to_celsius()
-    print("Enter c if you want to convert from Fahrenheit to Celsius")
-    print("Enter F if you want to convert from Celsius to Fahrenheit")
-    user_input = input()
-    if user_input == "c":
-        convert_celsius_to_fahrenheit
-    elif user_input == "f":
-        convert_fahrenheit_to_celsius
-    else: 
-        print("Incorrect input. Please try again later")
-    
-main()
